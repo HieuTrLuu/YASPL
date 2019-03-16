d53f59b82cff2b90cde813087c2817bf637bbd48
HieuTrLuu <thl1g15@soton.ac.uk>
2019-03-16 22:03:08 +0000

    fuck...

--- plc/Main.hs
+++ plc/Main.hs
@@ -14,18 +14,7 @@ main = do
 -- data Type = TInt | TFloat | TBool | TList Type | TPair Type Type | TFun Type Type
 -- type Environment = [(String, Expr)]
 -- type TEnvironment = [(String, Type)]
-data Frame = HCompare Expr Environment 
-           | CompareH Expr
-           | HAdd Expr Environment | AddH Expr
-           | HPair Expr Environment | PairH Expr
-           | FstH | SndH 
-           | HeadH | TailH
-           | HIf Expr Expr | HLet String ToyType Expr 
-           | HList Expr Environment
-           | HApp Expr Environment | AppH Expr
            
-type Kontinuation = [ Frame ]
-type State = (Expr,Environment,Kontinuation)
 
 
 
@@ -68,39 +57,11 @@ isValue (Ident a) = True
 isValue (Cl _ _ _ ) = True
 isValue _ = False
 
-
---Small step evaluation function
--- eval :: Expr -> Environment -> Expr
--- eval (Int_ a) -> Int_ a
--- eval (Float_ a) -> Float_ a
--- eval (True_) -> True_
--- eval (False_) -> False_
--- eval (List a) -> List {eval b | b <- a}
--- eval (Pair (a, b) = Pair (eval a, eval b))
-
--- output :: Expr -> Environment -> IO ()
-
--- read :: IO ()
--- read = do word <- sgetLine
-
 -- Function to iterate the small step reduction to termination
-evalLoop :: Expr -> Expr 
-evalLoop e = evalLoop' (e,[],[])
-  where evalLoop' (e,env,k) = if (e' == e) && (isValue e') then e' else evalLoop' (e',env',k')
-                       where (e',env',k') = eval1 (e,env,k) 
-
-eval1 :: State -> State
---this is where it is going to check the previous declared var
--- Rule for terminated evaluations
-eval1 (v,env,[]) | isValue v = (v,env,[])
-
-
-
-
-
-
-
-
+-- evalLoop :: Expr -> Expr 
+-- evalLoop e = evalLoop' (e,[],[])
+--   where evalLoop' (e,env,k) = if (e' == e) && (isValue e') then e' else evalLoop' (e',env',k')
+--                        where (e',env',k') = eval1 (e,env,k) 
 
 
 
