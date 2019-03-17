906812e91163ffe38ce8d8544bd8989f8820a323
HieuTrLuu <thl1g15>
2019-03-17 18:18:35 +0000

    finally finished fixing syntax error, going to debug again

--- plc/Main.hs
+++ plc/Main.hs
@@ -9,6 +9,7 @@ main = do
      t <- pure (alexScanTokens f)
      g <- pure (parseStreamLang t)
      print g
+     print (evalProg g [])
 
 -- i have declared these in Grammar.y
 -- data Type = TInt | TFloat | TBool | TList Type | TPair Type Type | TFun Type Type
@@ -93,12 +94,10 @@ eval (Add (Var str1) e2) env = Add (fst (getValueBinding str1 env)) e2
 
 eval (Add e1 (Var str2)) env = Add e1 (fst (getValueBinding str2 env))
 
+eval (Var str) env = fst (getValueBinding str env) 
 
 
 --TODO: list comprehension
-
-
-
 
 -- TODO: type checking
 -- unpack :: Expr -> Environment -> (Expr,Environment)
@@ -123,14 +122,14 @@ evalStatement (Assign (DivVal str e)) env = (Assign (Def str return), update env
 
 evalStatement (Assign (Def str e)) env = (Assign (Def str e), update env str e)
 
--- evalStatement (Return (x:xs)) env = evalStatement (Return (eval x env):(eval xs )) env
+evalStatement (Return e) env = (Return e, env)
 
 -- evalBlock :: Block -> Environment -> Block
 -- eavlBlock block env = [ (evalStatement x env) | x <- block]
 
 -- evalProg :: Prog -> Environment -> Environment
 evalProg :: Prog -> Environment -> Prog
-evalProg (x:xs) env = [ (str, (evalSection block env)) | (str,block) <- xs ]
+evalProg prog env = [ (str, (evalSection block env)) | (str,block) <- prog ]
 
 
 
@@ -138,8 +137,62 @@ evalProg (x:xs) env = [ (str, (evalSection block env)) | (str,block) <- xs ]
 evalSection :: Block -> Environment -> Block
 evalSection block env = [fst (evalStatement statment env) | statment <- block ]
 
+-- unparse :: Prog -> String
+-- unparse (x:xs) = (unparse [x]) ++ "\n" ++ (unparse xs)
+-- unparse 
+
+eval' :: (Expr, Environment) -> (Expr, Environment)
+eval' ((Int_ a), env) = (Int_ a, env)
+eval' ((Float_ a), env) = (Float_ a, env)
+eval' ((True_), env) = (True_, env)
+eval' ((False_), env) = (False_, env)
+    
+eval' ((List (x:xs)), []) | isValue (List (x:xs)) = ((List (x:xs)), [])
+                          | otherwise = error "List is not valid"
+-- not sure about the (eval' List xs env)
+eval' ((List ((Var str):xs)), env) = (List (fst (getValueBinding str env):(fst $ eval' ((List xs), env)):[]), snd (getValueBinding str env))
+        
+eval' ((Pair e1 e2), []) | isValue (Pair e1 e2) = ((Pair e1 e2), [])
+                         | otherwise = error "Pair is not valid"
+
+eval' ((Pair (Var str1) e), env) | (isValue e) = (Pair (fst (getValueBinding str1 env)) e, (snd (getValueBinding str1 env)))
+                             | otherwise = error "elt 1 of Pair is not valid"
+
+eval' ((Pair e (Var str2)), env) | (isValue e) = (Pair e (fst (getValueBinding str2 env)), (snd (getValueBinding str2 env)))
+                             | otherwise = error "elt 2 of Pair is not valid"
+
+eval' ((Add (Int_ a) (Int_ b)), env) = ((Int_ (a+b)), env)
+
+eval' ((Add (Var str1) e2), env) = (Add (fst (getValueBinding str1 env)) e2, (snd (getValueBinding str1 env)))
+
+eval' ((Add e1 (Var str2)), env) = (Add e1 (fst (getValueBinding str2 env)), (snd (getValueBinding str2 env)))
+
+eval'((Var str), env) = (getValueBinding str env) 
+
+evalStatement' :: (Statement, Environment) -> (Statement, Environment)
+evalStatement' ((Assign (Inc str e)), env) = (Assign (Def str return) ,update env str return)
+  where expr = fst (getValueBinding str env)
+        return = Add expr e
+
+evalStatement' ((Assign (Dec str e)),env) = (Assign (Def str return), update env str return)
+  where expr = fst (getValueBinding str env)
+        return = Sub expr e
+
+evalStatement' ((Assign (MultVal str e)), env) = (Assign (Def str return), update env str return)
+  where expr = fst (getValueBinding str env)
+        return = Mult expr e
+
+evalStatement' ((Assign (DivVal str e)), env) = (Assign (Def str return), update env str return)
+  where expr = fst (getValueBinding str env)
+        return = Div expr e
+
+evalStatement' ((Assign (Def str e)), env) = (Assign (Def str e), update env str e)
+
+evalStatement' ((Return e), env) = (Return e, env)
 
 
+evalSection' :: (Block, Environment) -> (Block, Environment)
+evalSection' ((x:xs),env) = evalSection' (xs,(snd $ evalStatement' (x,env)))
 
-
-
+evalProg' :: (Prog, Environment) -> (Prog, Environment)
+evalProg' ((x:xs),env) = evalProg' (xs,(snd $ evalSection' (snd x,env)))
